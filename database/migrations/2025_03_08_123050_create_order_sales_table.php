2331569ff4528b94cd2d16ea4d8e6ac20527b181
Guilherme <cabral.guilherme@gmail.com>
2025-03-08 09:33:39 -0300

    Create database migrations

---
+++ database/migrations/2025_03_08_123050_create_order_sales_table.php
@@ -0,0 +1,32 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderSalesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_sales', function (Blueprint $table) {
+            $table->id();
+            $table->string('customer_name');
+            $table->decimal('total_amount', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_sales');
+    }
+}
